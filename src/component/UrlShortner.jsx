0cf85941a0c0d74338660e96a906c5f73add6a06
Ketan Gupta <guptaketan6375@gmail.com>
2023-07-18 00:39:56 +0530

    Done

--- src/component/UrlShortner.jsx
+++ src/component/UrlShortner.jsx
@@ -38,15 +38,16 @@ export default function UrlShortner() {
                 shortUrl: ""
             })
         }
-        if (e.target.value === "redirect") {
-            const url = formData.shortUrl
-            if (url) {
-                window.open(JSON.parse(formData.shortUrl), '_blank')
-            } else {
-                alert("Url Not Generated")
-            }
-        }
     }
+
+    const handleRedirect =() => {
+        const url = formData.shortUrl
+        if (url) {
+            window.open(JSON.parse(formData.shortUrl), '_blank')
+        } else {
+            alert("Url Not Generated")
+        }
+    };
     const handleCopy = async () => {
         try {
             const textToCopy = textRef.current.innerText;
@@ -97,7 +98,7 @@ export default function UrlShortner() {
                     <path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z" />
                 </svg></button>
                 <hr />
-                <button className='btn' type="redirect" onClick={handelOnClick} value="redirect">
+                <button className='btn' type="redirect" onClick={handleRedirect} value="redirect">
                     <svg
                         height={24}
                         width={24}
